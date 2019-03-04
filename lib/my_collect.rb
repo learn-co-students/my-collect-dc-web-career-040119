fcb2e084dd57c119b2313cec880caa9a7f3bb590
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 20:52:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
 
 
+def my_collect(collection)
+  array = []
+   i=0
+  while i < collection.length
+    new_collect = yield(collection[i])
+    array << new_collect
+      i+=1
+   end
+   array
+end
